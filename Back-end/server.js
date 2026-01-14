fafe0fa441d0b2516c8191d8bd67d7458a2a56f2
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2026-01-14 10:29:34 +0530

    backend landing page added

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,10 +3,21 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import dbconnect from './Database/dbconnection.js';
 import router from './Routes/AuthRoutes/Routes.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+// static html
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Middleware
 app.use(cors());
